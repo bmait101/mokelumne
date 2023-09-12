938fd3ad6e81b5f51c8ea45d56d6d22b68154962
Bryan M <bmaitland101@gmail.com>
2023-09-12 13:05:10 -0500

    update theme

--- R/theme-moke.R
+++ R/theme-moke.R
@@ -11,9 +11,11 @@ theme_moke <- function(base = 8, family = font) {
     theme(
       axis.title.x = element_text(margin = margin(0,0,0,0)),
       axis.title.y = element_text(margin = margin(r=5)),
+      axis.text = element_text(color = "black"),
+      axis.ticks = element_line(),
       panel.grid = element_blank(),
       panel.background = element_blank(),
-      panel.border = element_rect(colour="black", fill=NA, size=.25), 
+      panel.border = element_rect(colour="black", fill=NA, linewidth=.25), 
       plot.background = element_rect(fill = "white", color = NA)
     )
 }
