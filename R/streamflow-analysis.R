86c36df24f081e1a7d93da43293693f9eace2a36
Bryan M <bmaitland101@gmail.com>
2023-11-08 13:49:06 -0600

    moving to qmd and updates

--- R/streamflow-analysis.R
+++ R/streamflow-analysis.R
@@ -14,103 +14,103 @@ library(mapview)
 
 ## Pull data from NWIS =====
 
-# # Create function to download NWIS data and convert to metric for use with fasstr
-# readNWISdv_clean <- function(x){
-#   # pull daily streamflow data for given site numbers
-#   data <- readNWISdv(
-#     siteNumbers = x, 
-#     parameterCd = "00060"  # discharge param code
-#   )
-#   
-#   # tidy up
-#   data <- data |> select(-agency_cd)
-#   names(data)[names(data) == 'site_no'] <- 'STATION_NUMBER'
-#   names(data)[names(data) == 'X_00060_00003'] <- 'Value'
-#   names(data)[names(data) == 'X_00060_00003_cd'] <- 'Symbol'
-#   
-#   # convert cfs to cms
-#   data$Value <- data$Value * 0.028317
-#   
-#   # get basin areas
-#   basin_area <- readNWISsite(siteNumbers = x)
-#   basin_area$Basin_Area_sqkm <- basin_area$drain_area_va * 2.58999
-#   basin_area <- data.frame(STATION_NUMBER = basin_area$site_no,
-#                            Basin_Area_sqkm = basin_area$Basin_Area_sqkm)
-#   # merge basin area back to data
-#   data <- merge(data, basin_area, by = "STATION_NUMBER")
-#   
-#   return(data)
-# }
-# 
-# # List of sites
-# site_numbers <- c(
-#   "11314000",  # TIGER C PH COND BL SALT SPRINGS DAM CA
-#   "11314500",  # NF MOKELUMNE R BL SALT SPRINGS DAM CA
-#   "11315000",  # COLE C NR SALT SPRINGS DAM CA
-#   "11315030",  # COLE C BL DIV DAM NR SALT SPRINGS DAM CA
-#   "11315900",  # BEAR R BL LO BEAR R DAM CA
-#   "11316100",  # BEAR R BL BEAR R DIV DAM CA
-#   "11316670",  # NF MOKELUMNE R BL TIGER C RES NR WEST PT CA
-#   # "11317000",  # MF MOKELUMNE R A WEST POINT CA
-#   # "11318500",  # SF MOKELUMNE R NR WEST POINT CA 
-#   "11319500",  # MOKELUMNE R NR MOKELUMNE HILL CA (above Parde Res)
-#   "11325500"  # MOKELUMNE R A WOODBRIDGE CA (below Camanche Res, near Lodi)
-# )
-# 
-# # download site metadata
-# metadata_sites <- 
-#   readNWISsite(siteNumbers = site_numbers) |> 
-#   as_tibble()
-# 
-# # download daily discharge data
-# data_usgs <- 
-#   readNWISdv_clean(site_numbers)
-# 
-# # check data download
-# head(metadata_sites)
-# head(data_usgs)
-# 
-# # make a site number - site name xref
-# xref <- metadata_sites |> select(site_no, station_nm)
-# 
-# # Add names, water year and date variables
-# data_usgs <- data_usgs |> 
-#   fill_missing_dates(water_year_start = 10) |>
-#   add_date_variables()
-# 
-# data_usgs <- data_usgs |> 
-#   left_join(xref, by = c("STATION_NUMBER" = "site_no")) |>
-#   relocate(station_nm, .after = STATION_NUMBER)
-# 
-# # Convert to list for each site
-# data_usgs_lst <- data_usgs |> 
-#   group_by(STATION_NUMBER) |>  
-#   group_map(~.x, .keep = TRUE)
-# 
-# # Name list elements
-# names(data_usgs_lst) <- data_usgs |> 
-#   group_by(STATION_NUMBER) |> 
-#   group_map(~.y) |> 
-#   unlist()
-# 
-# # Assign basin areas
-# basin_areas <- c(
-#   "11314000" = NA,
-#   "11314500" = metadata_sites[2,"drain_area_va"]$drain_area_va,
-#   "11315000" = metadata_sites[3,"drain_area_va"]$drain_area_va,
-#   "11315030" = metadata_sites[4,"drain_area_va"]$drain_area_va,
-#   "11315900" = metadata_sites[5,"drain_area_va"]$drain_area_va,
-#   "11316100" = metadata_sites[6,"drain_area_va"]$drain_area_va,
-#   "11316670" = metadata_sites[7,"drain_area_va"]$drain_area_va,
-#   "11319500" = metadata_sites[8,"drain_area_va"]$drain_area_va,
-#   "11325500" = metadata_sites[9,"drain_area_va"]$drain_area_va
-# )
-# 
-# # save data
-# save(
-#   data_usgs, data_usgs_lst, metadata_sites, xref, basin_areas,
-#   file = here::here("out","data","usgs_flows.Rdata")
-#   )
+# Create function to download NWIS data and convert to metric for use with fasstr
+readNWISdv_clean <- function(x){
+  # pull daily streamflow data for given site numbers
+  data <- readNWISdv(
+    siteNumbers = x,
+    parameterCd = "00060"  # discharge param code
+  )
+
+  # tidy up
+  data <- data |> select(-agency_cd)
+  names(data)[names(data) == 'site_no'] <- 'STATION_NUMBER'
+  names(data)[names(data) == 'X_00060_00003'] <- 'Value'
+  names(data)[names(data) == 'X_00060_00003_cd'] <- 'Symbol'
+
+  # convert cfs to cms
+  data$Value <- data$Value * 0.028317
+
+  # get basin areas
+  basin_area <- readNWISsite(siteNumbers = x)
+  basin_area$Basin_Area_sqkm <- basin_area$drain_area_va * 2.58999
+  basin_area <- data.frame(STATION_NUMBER = basin_area$site_no,
+                           Basin_Area_sqkm = basin_area$Basin_Area_sqkm)
+  # merge basin area back to data
+  data <- merge(data, basin_area, by = "STATION_NUMBER")
+
+  return(data)
+}
+
+# List of sites
+site_numbers <- c(
+  "11314000",  # TIGER C PH COND BL SALT SPRINGS DAM CA
+  "11314500",  # NF MOKELUMNE R BL SALT SPRINGS DAM CA
+  "11315000",  # COLE C NR SALT SPRINGS DAM CA
+  "11315030",  # COLE C BL DIV DAM NR SALT SPRINGS DAM CA
+  "11315900",  # BEAR R BL LO BEAR R DAM CA
+  "11316100",  # BEAR R BL BEAR R DIV DAM CA
+  "11316670",  # NF MOKELUMNE R BL TIGER C RES NR WEST PT CA
+  # "11317000",  # MF MOKELUMNE R A WEST POINT CA
+  # "11318500",  # SF MOKELUMNE R NR WEST POINT CA
+  "11319500",  # MOKELUMNE R NR MOKELUMNE HILL CA (above Parde Res)
+  "11325500"  # MOKELUMNE R A WOODBRIDGE CA (below Camanche Res, near Lodi)
+)
+
+# download site metadata
+metadata_sites <-
+  readNWISsite(siteNumbers = site_numbers) |>
+  as_tibble()
+
+# download daily discharge data
+data_usgs <-
+  readNWISdv_clean(site_numbers)
+
+# check data download
+head(metadata_sites)
+head(data_usgs)
+
+# make a site number - site name xref
+xref <- metadata_sites |> select(site_no, station_nm)
+
+# Add names, water year and date variables
+data_usgs <- data_usgs |>
+  fill_missing_dates(water_year_start = 10) |>
+  add_date_variables()
+
+data_usgs <- data_usgs |>
+  left_join(xref, by = c("STATION_NUMBER" = "site_no")) |>
+  relocate(station_nm, .after = STATION_NUMBER)
+
+# Convert to list for each site
+data_usgs_lst <- data_usgs |>
+  group_by(STATION_NUMBER) |>
+  group_map(~.x, .keep = TRUE)
+
+# Name list elements
+names(data_usgs_lst) <- data_usgs |>
+  group_by(STATION_NUMBER) |>
+  group_map(~.y) |>
+  unlist()
+
+# Assign basin areas
+basin_areas <- c(
+  "11314000" = NA,
+  "11314500" = metadata_sites[2,"drain_area_va"]$drain_area_va,
+  "11315000" = metadata_sites[3,"drain_area_va"]$drain_area_va,
+  "11315030" = metadata_sites[4,"drain_area_va"]$drain_area_va,
+  "11315900" = metadata_sites[5,"drain_area_va"]$drain_area_va,
+  "11316100" = metadata_sites[6,"drain_area_va"]$drain_area_va,
+  "11316670" = metadata_sites[7,"drain_area_va"]$drain_area_va,
+  "11319500" = metadata_sites[8,"drain_area_va"]$drain_area_va,
+  "11325500" = metadata_sites[9,"drain_area_va"]$drain_area_va
+)
+
+# save data
+save(
+  data_usgs, data_usgs_lst, metadata_sites, xref, basin_areas,
+  file = here::here("out","data","usgs_flows.Rdata")
+  )
 
 # load data
 load(here::here("out","data","usgs_flows.Rdata"))
@@ -213,7 +213,7 @@ mapshot(m3, file = here::here("out", "maps", "usgs-gages_local.png"))
 
 ## North Fork Mokelumne ----------
 
-df <- data_usgs_lst[[2]]
+df <- data_usgs_lst[[3]]
 
 plot_flow_data(df)
 plot_missing_dates(df)
@@ -228,10 +228,11 @@ plot_annual_extremes(df, water_year_start = 10)
 plot_annual_means(df, water_year_start = 10)
 plot_annual_normal_days(df, water_year_start = 10)
 
-plot_flow_duration(df, water_year_start = 10)
-plot_flow_duration(df, water_year_start = 10, end_year = 1980)
-plot_flow_duration(df, water_year_start = 10, start_year = 1990)
+plot_flow_duration(df, water_year_start = 10, ignore_missing = TRUE)
+plot_flow_duration(df, water_year_start = 10, ignore_missing = TRUE, end_year = 1980)
+plot_flow_duration(df, water_year_start = 10, ignore_missing = TRUE, start_year = 1990)
 
 
-plot_daily_stats(df, water_year_start = 10, end_year = 1980)
-plot_daily_stats(df, water_year_start = 10, start_year = 1990)
+plot_daily_stats(df, water_year_start = 10, ignore_missing = TRUE)
+plot_daily_stats(df, water_year_start = 10, ignore_missing = TRUE, end_year = 1980)
+plot_daily_stats(df, water_year_start = 10, ignore_missing = TRUE, start_year = 1990)
